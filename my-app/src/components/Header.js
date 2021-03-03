d3584093818d8eed429053ee62e4de073b9c4f3b
dashwood <laurentdashwood@gmail.com>
2021-03-03 15:18:07 +0100

    initilation

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import { ReactComponent } from "*.svg";
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+
+        return(
+            <div>hey</div>
+        )
+       
+    }
+};
+
+export default Header;
